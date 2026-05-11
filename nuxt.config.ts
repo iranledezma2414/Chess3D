6ac15d5ce6ce6cb41a98edac6d92996f799153fd
Iran Ledezma <iranledezma99@icloud.com>
2026-05-10 23:08:47 -0600

    Add Nitro configuration to disable pre-rendering and support 3D rendering

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,15 @@
 export default defineNuxtConfig({
   modules: [
     '@nuxtjs/tailwindcss'
-  ]
+  ],
+  build: {
+    transpile: ['three']
+  },
+  // Desactiva el pre-renderizado para evitar que intente cargar 3D en el servidor
+  nitro: {
+    prerender: {
+      crawlLinks: false,
+      routes: []
+    }
+  }
 })
